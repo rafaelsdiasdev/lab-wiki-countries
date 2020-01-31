5bf2d4686b99805df40cdfdf0112a0b5345750b4
Rafael Dias <rafaels.dias@me.com>
2020-01-31 10:56:23 -0300

    done

---
+++ starter-code/src/components/CountryDetail.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+const CountryDetails = props => {
+    const id = props.match.params.id;
+    const country = props.countries.find(el => el.cca3 === id);
+
+    return (
+        <div className="col-7">
+            <h1>
+                {country.flag} {country.name.common}
+            </h1>
+            <table className="table">
+                <thead></thead>
+                <tbody>
+                    <tr>
+                        <td style={{ width: "30%" }}>Capital</td>
+                        <td>{country.capital[0]}</td>
+                    </tr>
+                    <tr>
+                        <td>Area</td>
+                        <td>
+                            {country.area} km
+              <sup>2</sup>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Borders</td>
+                        <td>
+                            <ul>
+                                {country.borders.map(item => {
+                                    const atualCountry = props.countries.find(el => el.cca3 === item);
+                                    return (
+                                        <li key={item}>
+                                            <NavLink to={`/${item}`}>
+                                                {atualCountry.flag} {atualCountry.name.common}
+                                            </NavLink>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default CountryDetails;
---
+++ starter-code/src/components/Home.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Switch, Route, NavLink } from "react-router-dom";
+import Axios from "axios";
+import CountryDetail from "./CountryDetail";
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: []
+    };
+  }
+  componentDidMount() {
+    Axios.get("https://raw.githubusercontent.com/mledoze/countries/master/countries.json")
+      .then(result => {
+        this.setState({ countries: result.data });
+      })
+      .catch(err => console.error(err));
+  }
+  render() {
+    // console.log(this.state.countries[249])
+    return (
+<div>
+
+<nav className="navbar navbar-dark bg-primary mb-3">
+<div className="container">
+  <a className="navbar-brand" href="/">
+    WikiCountries
+  </a>
+</div>
+</nav>
+<div className="container">
+<div className="row">
+  <div className="col-5" style={{ maxHeight: "90vh", overflow: "scroll" }}>
+    <div className="list-group">
+      {this.state.countries.map(item => (
+        <NavLink key={item.cca3} className="list-group-item list-group-item-action" to={`/${item.cca3}`}>
+          {item.flag} {item.name.common}
+        </NavLink>
+      ))}
+    </div>
+  </div>
+        
+                    <Switch>
+              <Route
+                path="/:id"
+                render={props =>
+                  this.state.countries.length > 0 ? <CountryDetail countries={this.state.countries} {...props} />: null
+                }
+              />
+            </Switch>
+</div>
+</div>
+</div>
+
+    )
+  }
+}
+
+export default Home
